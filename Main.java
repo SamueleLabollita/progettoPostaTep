5841df99c4d7bcebc0287cca86871663473e53ba
SamueleLabollita <101709291+SamueleLabollita@users.noreply.github.com>
2023-04-17 12:34:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+public static void main(String[] args) throws InterruptedException {
 
-        Posta posta = new Posta();
-        Finestra finestra = new Finestra();
+    Posta posta = new Posta();
+    Finestra finestra = new Finestra();
 
-        Thread operatore1 = new Thread(new Operatore(posta));
-        Thread operatore2 = new Thread(new Operatore(posta));
+    Thread operatore1 = new Thread(new Operatore(posta, finestra));
+    Thread operatore2 = new Thread(new Operatore(posta, finestra));
 
-        Thread cliente1 = new Thread(new Cliente(posta));
-        Thread cliente2 = new Thread(new Cliente(posta));
+    Thread cliente1 = new Thread(new Cliente(posta, finestra));
+    Thread cliente2 = new Thread(new Cliente(posta, finestra));
 
-        operatore1.start();
-        operatore2.start();
+    operatore1.start();
+    operatore2.start();
 
-        cliente1.start();
-        cliente2.start();
+    cliente1.start();
+    cliente2.start();
 
-        finestra.log("Simulatore Posta avviato");
+    finestra.log("Simulatore Posta avviato");
 
-    }
+}
 }
