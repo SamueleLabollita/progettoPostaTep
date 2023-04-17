e1205caeaf5891841a12bba26857c78f5ac062c0
SamueleLabollita <101709291+SamueleLabollita@users.noreply.github.com>
2023-04-17 12:33:48 +0200

    Update Operatore.java

--- Operatore.java
+++ Operatore.java
@@ -1,21 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Operatore implements Runnable {
 
     private Posta posta;
-
-    public Operatore(Posta posta) {
+    private Finestra finestra;
+    
+    public Operatore(Posta posta, Finestra finestra) {
         this.posta = posta;
+        this.finestra = finestra;
     }
-
+    
     @Override
     public void run() {
         while (!Thread.interrupted()) {
             try {
                 Lettera lettera = posta.prelevaLettera();
                 if (lettera != null) {
+                    finestra.aggiungiCerchio(new CerchioCliente(375, 75, Color.BLUE)); // secondo sportello
                     System.out.println("Operatore ha preso la lettera " + lettera.getId());
                     Thread.sleep((long) (Math.random() * 8000 + 2000)); // tempo casuale tra 2 e 10 secondi
                     posta.spedisciLettera(lettera);
+                    finestra.rimuoviCerchio(new CerchioCliente(75, 75, Color.RED)); // primo sportello
+                    finestra.rimuoviCerchio(new CerchioCliente(375, 75, Color.BLUE)); // secondo sportello
                     System.out.println("Operatore ha spedito la lettera " + lettera.getId());
                 }
             } catch (InterruptedException e) {
@@ -24,4 +31,3 @@ public class Operatore implements Runnable {
         }
     }
 }
-
