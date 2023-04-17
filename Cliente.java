343f26608eeb9371a339100cfd9e90732440deb1
SamueleLabollita <101709291+SamueleLabollita@users.noreply.github.com>
2023-04-17 12:32:15 +0200

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Cliente implements Runnable {
 
     private Posta posta;
-
-    public Cliente(Posta posta) {
+    private Finestra finestra;
+    
+    public Cliente(Posta posta, Finestra finestra) {
         this.posta = posta;
+        this.finestra = finestra;
     }
-
+    
     @Override
     public void run() {
         while (!Thread.interrupted()) {
@@ -13,6 +18,7 @@ public class Cliente implements Runnable {
                 Thread.sleep((long) (Math.random() * 4000 + 1000)); // tempo casuale tra 1 e 5 secondi
                 Lettera lettera = new Lettera();
                 posta.inserisciLettera(lettera);
+                finestra.aggiungiCerchio(new CerchioCliente(75, 75, Color.RED)); // primo sportello
                 System.out.println("Cliente ha inserito la lettera " + lettera.getId());
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
